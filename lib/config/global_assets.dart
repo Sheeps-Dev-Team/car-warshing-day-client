a6d11d21bddd8644901cb33ecfdc92bebb439753
Eden0622 <84838438+Eden0622@users.noreply.github.com>
2023-12-11 11:04:00 +0900

    로그인, 프로필 페이지 작업중

--- lib/config/global_assets.dart
+++ lib/config/global_assets.dart
@@ -1,8 +1,15 @@
 class GlobalAssets {
   static const String svgLogo = 'assets/images/global/logo.svg'; // 로고
-  static const String svgArrowLeft = 'assets/images/global/arrow_left.svg'; // 왼쪽 화살표
+  static const String svgArrowLeft =
+      'assets/images/global/arrow_left.svg'; // 왼쪽 화살표
   static const String svgHome = 'assets/images/global/home.svg'; // 홈
   static const String svgCalendar = 'assets/images/global/calendar.svg'; // 캘린더
   static const String svgPerson = 'assets/images/global/person.svg'; // 사람
   static const String svgSnow = 'assets/images/global/snow.svg'; // 눈
+  static const String svgKakaoLogin =
+      'assets/images/global/kakao_login_btn.svg'; // 카카오로그인
+  static const String svgAppleLogin =
+      'assets/images/global/apple_login_btn.svg';
+  static const String svgGoogleLogin = // 애플로그인
+      'assets/images/global/google_login_btn.svg'; // 구글로그인
 }
