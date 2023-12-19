d878dce229e40c120818ebd5bae29dfdfa3c8941
noah <jsk9106@gmail.com>
2023-12-19 15:02:55 +0900

    login

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -15,7 +15,10 @@ const int genderFemale = 1; // 여
 
 const String division = '|'; // 문자열 구분자
 
-enum LoginType{kakao, apple, google, none} // 로그인 타입 (카카오, 애플, 구글, 비로그인)
+// enum LoginType{kakao, apple, google, none} // 로그인 타입 (카카오, 애플, 구글, 비로그인)
+const String loginTypeKaKao = '카카오';
+const String loginTypeApple = '애플';
+const String loginTypeGoogle = '구글';
 
 const int defaultPop = 30; // 기본 강수 확률 (사용자 설정 강수 확률 없을 시 쓰임)
 
